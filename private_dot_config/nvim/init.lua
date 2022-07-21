5d6efed8d3e16cbc5fcfe759607a086c08028dd3
Phillip Nguyen <philmein23@Phil-Nguyen-V3C6QT57TT.local>
2022-07-21 15:49:43 -0700

    update nvim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -138,16 +138,16 @@ vim.o.winbar = "%=%m %f"
 -- }
 --
 
+-- Tokyo night theme configration
+vim.g.tokyonight_style = "dark"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 --Set colorscheme
 vim.o.termguicolors = true
 vim.cmd [[colorscheme tokyonight]]
--- Tokyo night theme configration
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 --Set statusbar
 local navic = require("nvim-navic")
