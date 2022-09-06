a2b2610c3ce99dd94397ee0df2d633034e73de60
Phillip Nguyen <philmein23@Phil-Nguyen-V3C6QT57TT.local>
2022-09-06 15:39:03 -0700

    update nvim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -532,7 +532,7 @@ end)
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').current_buffer_fuzzy_find)
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string)
 vim.keymap.set('n', '<leader>fs', require('telescope.builtin').treesitter)
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>fg', require('telescope').extensions.live_grep_args.live_grep_args)
 vim.keymap.set('n', '<leader>fo', function()
   require('telescope.builtin').tags { only_current_buffer = true }
 end)
