1faab78ba194d021eb9fff02d3e0b96cb2a90c17
Phillip Nguyen <philmein23@Phil-Nguyen-V3C6QT57TT.local>
2022-09-09 11:01:53 -0700

    update nvim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -77,6 +77,10 @@ use {
 
 use 'folke/tokyonight.nvim'
 use {'ThePrimeagen/harpoon'}
+use {
+  'phaazon/hop.nvim',
+  branch = 'v2.0', -- optional but strongly recommended
+}
 end)
 
 --Set highlight on search
@@ -872,3 +876,11 @@ neogit.setup({
 })
 vim.keymap.set('n', '<leader>g', '<cmd>Neogit<cr>')
 
+
+-- hop
+local hop = require('hop')
+hop.setup({})
+vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
+vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
