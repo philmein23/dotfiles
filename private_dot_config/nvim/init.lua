aceb18643168108bb34dfae92bdcb2cb7cb05309
Phillip Nguyen <philmein23@Phil-Nguyen-V3C6QT57TT.local>
2022-09-09 17:46:25 -0700

    update nvim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -884,3 +884,4 @@ vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction =
 vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
 vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
+vim.keymap.set('n', '<leader>hw', ':HopWord <cr>')
