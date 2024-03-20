52f9245cc151517d336564b033c54495eaa776fc
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2024-03-19 18:57:40 -0700

    Update init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -284,7 +284,7 @@ vim.keymap.set("n", "<leader>1", "<cmd>Grapple select index=1<cr>")
 vim.keymap.set("n", "<leader>2", "<cmd>Grapple select index=2<cr>")
 vim.keymap.set("n", "<leader>3", "<cmd>Grapple select index=3<cr>")
 vim.keymap.set("n", "<leader>4", "<cmd>Grapple select index=4<cr>")
-vim.keymap.set("n", "<leader>5", "<cmd>Grapple select index=55<cr>")
+vim.keymap.set("n", "<leader>5", "<cmd>Grapple select index=5<cr>")
 
 -- Diffview
 vim.keymap.set('n', '<leader>dvo', ':DiffviewOpen <CR>')
